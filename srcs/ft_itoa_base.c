b66d251a02b571b5b1d03ee31111e4528db8bff4
Sarah FRANC <sfranc@e2r5p18.42.fr>
2017-05-03 15:14:38 +0200

    Correction itoa base

--- srcs/ft_itoa_base.c
+++ srcs/ft_itoa_base.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_itoa_base.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: exam <marvin@42.fr>                        +#+  +:+       +#+        */
+/*   By: sfranc <sfranc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/10 10:45:33 by exam              #+#    #+#             */
-/*   Updated: 2017/01/11 16:38:19 by sfranc           ###   ########.fr       */
+/*   Updated: 2017/05/03 15:04:20 by sfranc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,15 @@ static int		value_size(int value, int base)
 	int	size;
 
 	size = 0;
-	if (value == 0)
-		++size;
+	if (base <= 1 || base >= 17)
+		return (0);
 	if (value < 0 && base == 10)
-		++size;
+		size++;
+	if (value == 0)
+		size++;
 	while (value != 0)
 	{
-		++size;
+		size++;
 		value /= base;
 	}
 	return (size);
@@ -31,27 +33,27 @@ static int		value_size(int value, int base)
 
 char			*ft_itoa_base(int value, int base)
 {
-	int			size;
-	long int	value_long;
+	static char	tab[16] = "0123456789ABCDEF";
 	char		*result;
-	char		*tab_base;
+	int			size;
+	long		nb;
 
-	if (base <= 1 || base >= 17)
-		return (NULL);
-	tab_base = ft_strdup("0123456789ABCDEF");
-	size = value_size(value, base);
-	if (!(result = ft_strnew(size)))
-		return (NULL);
-	result[0] = '0';
+	if (!(size = value_size(value, base)))
+		return (0);
+	if (!(result = (char *)malloc(sizeof(char) * (size + 1))))
+		ft_exit("Unable to malloc", 2);
+	result[size] = '\0';
+	if (value == 0)
+		result[0] = '0';
 	if (value < 0 && base == 10)
 		result[0] = '-';
-	value_long = (long)value;
-	if (value < 0)
-		value_long *= -1;
-	while (size--)
+	nb = (long)value;
+	if (nb < 0)
+		nb = nb * -1;
+	while (nb != 0 && size--)
 	{
-		result[size] = tab_base[value_long % base];
-		value_long /= base;
+		result[size] = tab[nb % base];
+		nb /= base;
 	}
 	return (result);
 }
